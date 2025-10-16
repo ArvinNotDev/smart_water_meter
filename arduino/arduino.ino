d2775556dea362f8dd022aa20dd07b9e003034f0
Arvin <arveenjafari19@gmail.com>
2025-10-16 19:55:28 +0330

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,18 @@
 #include <Wire.h>
-#include "IranGSM.h"
+#include "IranGSM.h" // this is a customized version of IranGSM.h
 
 #define RX_PIN A2
 #define TX_PIN A1
 #define PIN_HALL 2
+
+#define LED_R 5
+#define LED_G 6
+#define LED_B 7
+
 #define EEPROM_I2C_ADDR 0x50
 #define EEPROM_TOTAL_BYTES 32768UL
 #define EEPROM_PAGE_SIZE 64
+
 #define RECORD_SIZE 8
 #define RECORDS_COUNT (EEPROM_TOTAL_BYTES / RECORD_SIZE)
 #define COMMIT_HI 0xA5
@@ -21,8 +27,8 @@ IranGSM myGSM(RX_PIN, TX_PIN);
 
 volatile unsigned int pulseCounter = 0;
 volatile unsigned int literCounter = 0;
-
 unsigned int lastSavedLiters = 0;
+
 uint32_t lastSequence = 0;
 uint32_t eepromWriteIndex = 0;
 
@@ -35,15 +41,25 @@ struct SmsJob {
 SmsJob smsQ[SMS_Q_SZ];
 
 bool waitingForInitialLiters = true;
-
 volatile bool saveRequested = false;
 volatile uint16_t saveRequestedLiters = 0;
+
+bool gsmReady = false;
+unsigned long lastGsmCheck = 0;
+bool ledBlinkState = false;
+
+void setLED(bool r, bool g, bool b) {
+  digitalWrite(LED_R, r ? HIGH : LOW);
+  digitalWrite(LED_G, g ? HIGH : LOW);
+  digitalWrite(LED_B, b ? HIGH : LOW);
+}
 
 void countPulse() {
   static unsigned long lastMicros = 0;
   unsigned long now = micros();
   if (now - lastMicros < 2000UL) return;
   lastMicros = now;
+
   pulseCounter++;
   if (pulseCounter >= PULSES_PER_LITER) {
     pulseCounter = 0;
@@ -104,8 +120,10 @@ void eeprom_write_record(uint32_t recIndex, uint32_t seq, uint16_t liters) {
   rec[2] = (uint8_t)(seq >> 8);  rec[3] = (uint8_t)(seq);
   rec[4] = (uint8_t)(liters >> 8); rec[5] = (uint8_t)(liters);
   rec[6] = 0xFF; rec[7] = 0xFF;
+
   uint16_t addr = recAddr(recIndex);
   eeprom_write_pagewise(addr, rec, 6);
+
   rec[6] = COMMIT_HI; rec[7] = COMMIT_LO;
   eeprom_write_pagewise(addr + 6, rec + 6, 2);
 }
@@ -198,14 +216,17 @@ void processIncomingSms(const String &fromRaw, const String &bodyRaw) {
 
   String from = fromRaw;
   from.replace(" ", ""); from.replace("(", ""); from.replace(")", "");
+
   String trustedTail = TRUSTED_NUMBER;
   trustedTail.replace(" ", ""); trustedTail.replace("(", ""); trustedTail.replace(")", "");
   int tlen = trustedTail.length();
   int tailLen = (tlen >= 11) ? 11 : tlen;
   String trustedTailEnd = trustedTail.substring(tlen - tailLen);
+
   int flen = from.length();
   String fromTail = from;
   if (flen > tailLen) fromTail = from.substring(flen - tailLen);
+
   String body = bodyRaw;
   body.trim();
   String bodyUpper = body;
@@ -272,52 +293,96 @@ void processIncomingSms(const String &fromRaw, const String &bodyRaw) {
   }
 }
 
+bool checkGsmConnected() {
+  String resp = myGSM.sendAT("AT+CSQ", 2000);
+
+  int idx = resp.indexOf("+CSQ:");
+  if (idx == -1) return false;
+
+  int comma = resp.indexOf(',', idx);
+  if (comma == -1) return false;
+
+  int rssi = resp.substring(idx + 6, comma).toInt();
+
+  if (rssi >= 0 && rssi <= 31) return true;
+  return false;
+}
+
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
   delay(2000);
+
   pinMode(PIN_HALL, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PIN_HALL), countPulse, FALLING);
+
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+  setLED(false, false, false);
+
   delay(1500);
+
   if (DEBUG) Serial.println(F("=== Water meter starting (IranGSM mode) ==="));
+
   uint16_t startLit = loadLitersFromEEPROM();
   noInterrupts(); literCounter = startLit; interrupts();
   lastSavedLiters = startLit;
+
   if (lastSequence != 0) waitingForInitialLiters = false;
   else waitingForInitialLiters = true;
-  enqueueSms(TRUSTED_NUMBER, "START");
 }
 
 void loop() {
-  if (myGSM.available()) {
-    if (myGSM.is_SMS()) {
-      if (DEBUG) Serial.println(F("=====>   new SMS arrived (IranGSM)"));
-      String SMS_Text = myGSM.SMS_Read();
-      String Phone_Number = myGSM.SMS_Number();
-      processIncomingSms(Phone_Number, SMS_Text);
+  if (!gsmReady && millis() - lastGsmCheck >= 2000) {
+    lastGsmCheck = millis();
+    bool connected = checkGsmConnected();
+
+    if (connected) {
+      gsmReady = true;
+      setLED(false, true, false);
+      enqueueSms(TRUSTED_NUMBER, "START");
+      if (DEBUG) Serial.println(F("GSM connected, START sent"));
+    } else {
+      ledBlinkState = !ledBlinkState;
+      setLED(ledBlinkState, false, false);
+      if (DEBUG) Serial.println(F("GSM not connected, blinking red"));
     }
   }
 
-  serviceSmsOut();
-
-  uint16_t cur;
-  noInterrupts(); cur = literCounter; interrupts();
-  static uint16_t lastSnap = 0;
-  if (cur != lastSnap) {
-    lastSnap = cur;
-    if (DEBUG) { Serial.print(F("Liters: ")); Serial.println(cur); }
-    if ((cur / LITERS_PER_SAVE) != (lastSavedLiters / LITERS_PER_SAVE)) {
-      requestSave(cur);
+  if (gsmReady) {
+    if (myGSM.available()) {
+      if (myGSM.is_SMS()) {
+        if (DEBUG) Serial.println(F("=====>   new SMS arrived (IranGSM)"));
+        String SMS_Text = myGSM.SMS_Read();
+        String Phone_Number = myGSM.SMS_Number();
+        processIncomingSms(Phone_Number, SMS_Text);
+      }
     }
-  }
 
-  if (saveRequested) {
-    noInterrupts();
-    uint16_t litersToSave = saveRequestedLiters;
-    saveRequested = false;
-    interrupts();
-    saveLitersToEEPROM(litersToSave);
-    lastSavedLiters = litersToSave;
+    serviceSmsOut();
+
+    uint16_t cur;
+    noInterrupts(); cur = literCounter; interrupts();
+    static uint16_t lastSnap = 0;
+    if (cur != lastSnap) {
+      lastSnap = cur;
+      if (DEBUG) { Serial.print(F("Liters: ")); Serial.println(cur); }
+      if ((cur / LITERS_PER_SAVE) != (lastSavedLiters / LITERS_PER_SAVE)) {
+        requestSave(cur);
+      }
+    }
+
+    if (saveRequested) {
+      noInterrupts();
+      uint16_t litersToSave = saveRequestedLiters;
+      saveRequested = false;
+      interrupts();
+
+      saveLitersToEEPROM(litersToSave);
+      lastSavedLiters = litersToSave;
+    }
   }
 
   delay(100);
